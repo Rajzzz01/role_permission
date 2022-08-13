0bb1e8d7dbf070d6795bdbb69ffc77c09acad078
Raj <trajkumar@mindstask.com>
2022-08-13 18:10:06 +0530

    initial commit

---
+++ database/migrations/2022_08_13_110859_create_user_address_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_address', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('building_no')->nullable();
+            $table->string('street_name')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->string('pincode')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_address');
+    }
+}
